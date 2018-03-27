9ea91ad7262bcd4a85ab1f90c9e2f065929248ba
ImproTekniikat <pekka.suomala@gmail.com>
2018-03-27 13:27:54 +0300

    Added Auth0.

---
+++ client/src/Auth/Auth.js
@@ -0,0 +1,78 @@
+import history from '../history';
+import auth0 from 'auth0-js';
+import { AUTH_CONFIG } from './auth0-variables';
+
+export default class Auth {
+  auth0 = new auth0.WebAuth({
+    domain: AUTH_CONFIG.domain,
+    clientID: AUTH_CONFIG.clientId,
+    redirectUri: AUTH_CONFIG.callbackUrl,
+    audience: `https://${AUTH_CONFIG.domain}/userinfo`,
+    responseType: 'token id_token',
+    scope: 'openid profile'
+  });
+
+  constructor() {
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.setSession = this.setSession.bind(this);
+  }
+
+  login() {
+    this.auth0.authorize();
+  }
+
+  handleAuthentication() {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        //        this.setSession(authResult);
+        history.replace('/home');
+
+        this.auth0.client.userInfo(authResult.accessToken, (err, user) => {
+          console.log(JSON.stringify(user));
+          this.setSession(authResult, user);
+        });
+      } else if (err) {
+        history.replace('/home');
+        console.log(err);
+        alert(`Error: ${err.error}. Check the console for further details.`);
+      }
+    });
+  }
+
+  setSession(authResult, user) {
+    // Set the time that the access token will expire at
+    let expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+    localStorage.setItem('user', user.name);
+    // navigate to the home route
+    history.replace('/home');
+  }
+
+  logout() {
+    // Clear access token and ID token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    localStorage.removeItem('user');
+    // navigate to the home route
+    history.replace('/home');
+  }
+
+  isAuthenticated() {
+    // Check whether the current time is past the
+    // access token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
+  username() {
+    return localStorage.getItem('user');
+  }
+}
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import { Navbar, NavItem, Nav } from 'react-bootstrap';
 
-const Header = () => {
-  return (
-    <Navbar staticTop={true}>
-      <Navbar.Header>
-        <Navbar.Brand>Improsivusto </Navbar.Brand>
-      </Navbar.Header>
-      <Nav className="pull-right">
-        <NavItem eventKey={1} href="#">
-          Kirjaudu
-        </NavItem>
-      </Nav>
-    </Navbar>
-  );
-};
+class Header extends React.Component {
+  render() {
+    const { isAuthenticated, username } = this.props.auth;
+    const user = username();
+    return (
+      <Navbar staticTop={true}>
+        <Navbar.Header>
+          <Navbar.Brand>Improsivusto </Navbar.Brand>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Navbar.Text>
+            {isAuthenticated() && <span>Terve {user}!</span>}
+          </Navbar.Text>
+          <Nav className="pull-right">
+            {!isAuthenticated() && (
+              <NavItem eventKey={1} href="#" onClick={this.login.bind(this)}>
+                Kirjaudu
+              </NavItem>
+            )}
+            {isAuthenticated() && (
+              <NavItem eventKey={1} href="#" onClick={this.logout.bind(this)}>
+                Kirjaudu ulos
+              </NavItem>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 
+  goTo(route) {
+    this.props.history.replace(`/${route}`);
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+}
 export default Header;
